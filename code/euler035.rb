fc97436ac2efa95312ae63242add44c65d49c796
prusswan <prusswan@gmail.com>
2013-05-29 20:45:01 +0800

    improving p35 runtime with proper prime sieve

--- code/euler035.rb
+++ code/euler035.rb
@@ -1,37 +1,26 @@
-$primes = Hash.new
 limit = 1000000
+$primes = (0..limit).to_a
 
-counter = 0
+# using proper prime sieve
 
-# should use a proper prime sieve but we're (still) lazy
-
-(2..limit).each do |c|
-  is_prime = true
-  (2..(c**0.5).floor).each do |d|
-    if c%d==0
-      is_prime = false
-      break
-    end
-  end
-
-  if is_prime == true
-    $primes[c] = 1
-    counter+=1
+(2+2..limit).step(2) { |i| $primes[i] = false }
+(3..1000).step(2) do |c|
+  (c+c..limit).step(c) do |n|
+    $primes[n] = false
   end
 end
 
-
-
 # puts $primes.keys.max
+# puts $primes[2..10]
 r = []
 
-$primes.keys.each do |p|
+$primes[2..-1].select{ |e| e }.each do |p|
   s = p.to_s.chars.to_a
   t = s
   cyclic = true
   while t = t.rotate
     break if s == t
-    if not $primes.include? t.join.to_i
+    if $primes[t.join.to_i] == false
       cyclic = false
       break
     end
